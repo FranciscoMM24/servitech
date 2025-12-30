e6109c8cac14e67cfe637dd7e6471b652731cac6
Francisco1124 <jesusfco.morales@hotmail.com>
2025-12-30 15:33:40 -0700

    modificaciones en diseño

--- src/app/inicio/inicio.ts
+++ src/app/inicio/inicio.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
